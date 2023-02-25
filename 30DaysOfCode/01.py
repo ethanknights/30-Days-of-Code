df1d7b06d7f290fda335b3b1804f1aed91c2091e
ethanknights <ethanknights@hotmail.co.uk>
2023-02-25 09:45:14 +0000

    01

---
+++ 30DaysOfCode/01.py
@@ -0,0 +1,16 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+myInt = int(12)
+myDouble = float(4.0)
+myString = 'is the best place to learn and practice coding!'
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+print(myInt + i)
+# Print the sum of the double variables on a new line.
+print(myDouble + d)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s + myString)
