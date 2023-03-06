9b6aaccc921729fd9cbb1ec0a64589629d1e09b3
ethanknights <ethanknights@hotmail.co.uk>
2023-03-06 16:31:50 +0000

    day 06

---
+++ 30DaysOfCode/solution_06.py
@@ -0,0 +1,23 @@
+
+
+def splitEvenOdd(myStrings):
+
+    evenString = str(); oddString = str()
+
+    for idx, i in enumerate(myString):
+        if (idx % 2) == 0:
+            evenString += i
+        else:
+            oddString += i
+
+    return str(evenString), str(oddString)
+
+
+nStrings = int(input())
+for i in range(0, nStrings):
+    myString = str(input())
+
+    evenString, oddString = splitEvenOdd(myString)
+
+    concatString = str(evenString + ' ' + oddString)
+    print(concatString)
