b1d53408a13477aa47dd08abda7746e1f82b1093
ethanknights <ethanknights@hotmail.co.uk>
2023-03-14 14:44:42 +0000

    add solution_01.go

--- 30DaysOfCode/01-DataTypes/solution_01.go
+++ 30DaysOfCode/01-DataTypes/solution_01.go
@@ -1,19 +1,32 @@
 package main
+
 import (
-    "bufio"
-    "fmt"
-    "os"
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
 )
 
 func main() {
+	var _ = strconv.Itoa // Ignore this comment. You can still use the package "strconv".
 
-// var myString string // input
-// fmt.Println("Hello, world")
-// fmt.Scanln(&myString) // input
-// fmt.Println(myString) // input
+	var i uint64 = 4
+	var d float64 = 4.0
+	var s string = "HackerRank "
 
-reader := bufio.NewReader(os.Stdin)
-userText, _ := reader.ReadString('\n')
-fmt.Printf("Hello, World.\n%s", userText)
+	scanner := bufio.NewScanner(os.Stdin)
+
+	var myInt uint64
+	var myDouble float64
+	var myString string
+
+	fmt.Scan(&myInt)
+	fmt.Scan(&myDouble)
+	scanner.Scan()
+	myString = scanner.Text()
+
+	fmt.Println(i + myInt)
+	fmt.Printf("%.1f\n", d+myDouble)
+	fmt.Printf("%s%s", s, myString)
 
 }
