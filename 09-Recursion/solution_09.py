399e42764636b1a2d749aa7370eb08d05754918c
Ethan Knights <ethanknights@hotmail.co.uk>
2023-04-14 17:25:54 +0100

    enhance: tidy day9 py

--- 09-Recursion/solution_09.py
+++ 09-Recursion/solution_09.py
@@ -1,19 +1,11 @@
-# Compute factorial of a given integer using recursion
-
 val = int(input().strip())
 
-
 def calc_factorial(factorial):
-    # create range of vals to multiply by
     r = range(0, factorial, 1)
     r = r[::-1]
-
     for rr in r:
-        # print(rr)
         if rr != 0:
             factorial = factorial * rr
-            # print(factorial)
-
     return factorial
 
 
