5bfa88f0930ab73b0ded7ce25f7c16a1c3b72234
Ethan Knights <ethanknights@hotmail.co.uk>
2023-04-14 17:19:27 +0100

    enhance: tidy day1 py

--- 01-DataTypes/solution_01.py
+++ 01-DataTypes/solution_01.py
@@ -1,20 +1,11 @@
 i = 4
 d = 4.0
 s = "HackerRank "
-# Declare second integer, double, and String variables.
-myInt = None
-myDouble = None
-myString = None
 
-# Read and save an integer, double, and String to your variables.
 myInt = int(input())
 myDouble = float(input())
 myString = str(input())
 
-# Print the sum of both integer variables on a new line.
 print(myInt + i)
-# Print the sum of the double variables on a new line.
 print(myDouble + d)
-# Concatenate and print the String variables on a new line
-# The 's' variable above should be printed first.
 print(s + myString)
